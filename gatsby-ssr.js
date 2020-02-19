aaa13f17e67585414b694731f3afb9841f83f89d
Js-Brecht <myrlin1@gmail.com>
2020-02-19 09:36:46 -1000

    only load these endpoints if needed

--- gatsby-ssr.js
+++
@@ -1,5 +0,0 @@
-// This has to be faked, so that Gatsby will include this plugin's SSR file
-// when running SSR
-exports.onPreRenderHTML = () => null;
-
-module.exports = require('./dist/gatsby/gatsby-ssr');
