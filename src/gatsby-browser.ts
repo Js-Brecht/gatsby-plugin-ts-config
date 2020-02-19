a63543e8afeda8c4909f3913f51c63120d0ad58b
Jeremy Albright <myrlin1@gmail.com>
2020-02-18 18:05:57 -1000

    organized src files

--- src/gatsby-browser.ts
+++
@@ -1,7 +0,0 @@
-let gatsbyBrowser = {};
-try {
-    gatsbyBrowser = require(`${__TS_CONFIG_DIR}/gatsby-browser`);
-} catch (err) { // no gatsby-browser found, return nothing
-    // noop
-}
-export = gatsbyBrowser;
--- src/gatsby-ssr.ts
+++
@@ -1,8 +0,0 @@
-let gatsbySsr = {};
-try {
-    gatsbySsr = require(`${__TS_CONFIG_DIR}/gatsby-ssr`);
-} catch (err) { // no gatsby-ssr found, return nothing
-    // noop
-}
-
-export = gatsbySsr;
