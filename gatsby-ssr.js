1293b6c4727a8b4c986d980c7884b47481953974
Jeremy Albright <myrlin1@gmail.com>
2020-02-18 21:29:22 -1000

    documentation

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,5 @@
+// This has to be faked, so that Gatsby will include this plugin's SSR file
+// when running SSR
 exports.onPreRenderHTML = () => null;
 
 module.exports = require('./dist/gatsby/gatsby-ssr');
