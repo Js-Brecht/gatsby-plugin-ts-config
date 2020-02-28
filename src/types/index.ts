e150e0262134055f3e1f98ed527154d1c2bd6987
Jeremy Albright <myrlin1@gmail.com>
2020-02-27 18:27:10 -1000

    universal types

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+import { PluginOptions } from 'gatsby';
+import { TransformOptions } from '@babel/core';
+import { RegisterOptions } from 'ts-node';
+
+export interface ITsConfigArgs extends Omit<PluginOptions, 'plugins'> {
+    configDir?: string;
+    projectRoot?: string;
+    tsNode?: RegisterOptions;
+}
+
+export type IRegisterType = 'ts-node' | 'babel';
+export type IValidExts = '.js' | '.ts' | '.jsx' | '.tsx';
+export type IConfigTypes = 'config' | 'node' | 'browser' | 'ssr';
+export type IEndpointResolutionSpec = IConfigTypes | {
+    type: IConfigTypes;
+    ext: IValidExts[];
+}
+
+
+export type IRegisterOptions<T extends IRegisterType> =
+    T extends 'ts-node'
+        ? RegisterOptions
+        : T extends 'babel'
+            ? TransformOptions
+            : never;
+
+export interface ICommonDirectories {
+    projectRoot: string;
+    configDir: string;
+    cacheDir: string;
+}
+
+export interface IGlobalOpts extends ICommonDirectories {
+    endpoints: IGatsbyEndpoints;
+    ignore: IConfigTypes[];
+    transformOpts?: TransformOptions;
+}
+
+export type IGatsbyEndpoints = {
+    [k in IConfigTypes]?: string;
+}
