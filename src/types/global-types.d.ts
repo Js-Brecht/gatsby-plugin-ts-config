c7048440f53240fdd5d5eb01aaf0040c39a922c7
Jeremy Albright <myrlin1@gmail.com>
2020-02-29 15:55:42 -1000

    added module types for:
    * @babel/register
    * gastby-cli/lib/reporter

--- src/types/global-types.d.ts
+++ src/types/global-types.d.ts
@@ -1,2 +1,24 @@
 declare const __TS_CONFIG_ENDPOINT_PATH: string;
 declare const __TS_CONFIG_CACHE_DIR__: string;
+
+declare module '@babel/register' {
+    import { TransformOptions } from '@babel/core';
+    type IOnlyFn = (filename: string) => boolean;
+    export interface IRegisterOptions extends TransformOptions {
+        extensions?: string[];
+        only?: string | RegExp | IOnlyFn | null | Array<
+            | string
+            | RegExp
+            | IOnlyFn
+        >;
+    }
+    declare const register: (args: IRegisterOptions) => void;
+    export declare const revert: () => void;
+    export default register;
+}
+
+declare module "gatsby-cli/lib/reporter" {
+    import { Reporter } from 'gatsby';
+    const reporter: Reporter;
+    export default reporter;
+}
