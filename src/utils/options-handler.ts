f8a512eeea19a0e02db3bb51f37375c513c812c4
Jeremy Albright <myrlin1@gmail.com>
2020-02-29 15:53:49 -1000

    provide default options handler for babel

--- src/utils/options-handler.ts
+++ src/utils/options-handler.ts
@@ -1,4 +1,6 @@
 import { keys } from 'ts-transformer-keys';
+import mergeWith from 'lodash.mergewith';
+import { TransformOptions } from '@babel/core';
 import { IGlobalOpts, IPublicOpts } from "../types";
 
 const publicProps = keys<IPublicOpts>();
@@ -7,8 +9,11 @@ class OptionsHandler {
     private opts = {} as IGlobalOpts;
     private publicOpts = {} as IPublicOpts;
 
-    public set(args: IGlobalOpts) {
-        this.opts = args;
+    public set(args: Partial<IGlobalOpts>) {
+        this.opts = {
+            ...this.opts,
+            ...args,
+        };
         this.publicOpts = Object.entries(this.opts)
             .filter(([key]) => publicProps.includes(key as keyof IPublicOpts))
             .reduce((acc, [key, val]) => {
@@ -23,6 +28,29 @@ class OptionsHandler {
 
     public public(): IPublicOpts {
         return this.publicOpts;
+    }
+
+    private mergeOptionsWithConcat(to: any, from: any): any {
+        if (to instanceof Array) {
+            return to.concat(from);
+        }
+    }
+
+    public setBabelOpts(opts?: TransformOptions): Required<IGlobalOpts>['transformOpts'] {
+        this.opts.transformOpts = mergeWith(
+            {
+                sourceMaps: true,
+                sourceRoot: this.opts.projectRoot,
+                cwd: this.opts.projectRoot,
+                presets: [
+                    require.resolve('@babel/preset-typescript'),
+                    require.resolve('babel-preset-gatsby-package'),
+                ],
+            },
+            opts,
+            this.mergeOptionsWithConcat,
+        );
+        return this.opts.transformOpts;
     }
 }
 
