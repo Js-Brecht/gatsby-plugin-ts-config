385b989e90b265851ee7242f0e1ced4ed3af08eb
Jeremy Albright <myrlin1@gmail.com>
2020-03-11 19:45:11 -1000

    utility types for inferring gatsby endpoint return objects/modules

--- src/types/internal.ts
+++ src/types/internal.ts
@@ -1,5 +1,7 @@
+import { GatsbyConfig, GatsbyNode, GatsbyBrowser, GatsbySSR } from 'gatsby';
 import { TransformOptions } from '@babel/core';
 import { RegisterOptions } from 'ts-node';
+import { ITSConfigFn } from './public';
 
 export type IRegisterType = 'ts-node' | 'babel';
 export type IValidExts = '.js' | '.ts' | '.jsx' | '.tsx';
@@ -7,6 +9,34 @@ export type IConfigTypes = 'config' | 'node' | 'browser' | 'ssr';
 export type IEndpointResolutionSpec = IConfigTypes | {
     type: IConfigTypes;
     ext: IValidExts[];
+}
+
+export type IEndpointReturnTypes<T extends IConfigTypes = IConfigTypes> = T extends 'config'
+    ? GatsbyConfig | ITSConfigFn<'config'>
+    : T extends 'node'
+        ? GatsbyNode | ITSConfigFn<'node'>
+        : T extends 'browser'
+            ? GatsbyBrowser
+            : GatsbySSR
+
+export type IEndpointReturnObject<T extends IConfigTypes> = T extends 'config'
+    ? GatsbyConfig
+    : T extends 'node'
+        ? GatsbyNode
+        : T extends 'browser'
+            ? GatsbyBrowser
+            : GatsbySSR;
+
+export type InferredConfigType<T extends IEndpointReturnTypes, K = IConfigTypes> = T extends GatsbyConfig | ITSConfigFn<'config'>
+    ? 'config'
+    : T extends GatsbyNode | ITSConfigFn<'node'>
+        ? 'node'
+        : T extends GatsbyBrowser
+            ? 'browser'
+            : 'ssr';
+
+export type IGatsbyEndpoints = {
+    [k in IConfigTypes]?: string[];
 }
 
 export type IRegisterOptions<T extends IRegisterType> =
@@ -29,6 +59,3 @@ export interface IGlobalOpts extends ICommonDirectories {
     tsNodeOpts?: RegisterOptions;
 }
 
-export type IGatsbyEndpoints = {
-    [k in IConfigTypes]?: string[];
-}
