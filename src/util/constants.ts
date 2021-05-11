2cd9bee5c3fc220267841d85a0df909871d15d0f
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2021-05-11 11:16:57 -1000

    fix root pkgJson path.join

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -3,6 +3,6 @@ import type { PackageJson } from "type-fest";
 
 export const thisRoot = path.resolve(__dirname, "..", "..");
 export const pkgJson: PackageJson = require(
-    path.join(thisRoot + "package.json"),
+    path.join(thisRoot, "package.json"),
 );
 export const pluginName = pkgJson.name;
