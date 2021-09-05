e548b8791eaa08969f1de9bf9cf1fd664b5f0620
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2021-09-04 15:15:09 -1000

    remove "old" directory; not applicable on this version

--- old/utils/errors.ts
+++
@@ -1,7 +0,0 @@
-export const throwError = (message: string, err?: string | Error) => {
-    if (err instanceof Error) {
-        err.message = [message, err.message].join("\n");
-        throw err;
-    }
-    throw new Error([message, err].join("\n"));
-};
